79f3b5c021128ab8f6a39ca833f8ce191f219d4e
willclarktech <willclarktech@users.noreply.github.com>
2019-08-14 12:54:51 +0200

    Add basic travis setup

---
+++ scripts/travis/main.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+command -v shellcheck > /dev/null && shellcheck "$0"
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# shellcheck disable=SC1090
+source "$SCRIPT_DIR/_includes.sh";
+
+fold_start "yarn-install"
+retry 3 yarn install
+fold_end
+
+fold_start "yarn-build"
+yarn build
+fold_end

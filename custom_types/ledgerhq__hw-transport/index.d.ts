b3d430e236b6a43e66dc3e8e3c227fcf69d5d4d9
Simon Warta <simon@warta.it>
2019-08-28 11:06:47 +0200

    Add LedgerApp class

---
+++ custom_types/ledgerhq__hw-transport/index.d.ts
@@ -0,0 +1,16 @@
+declare module "@ledgerhq/hw-transport" {
+  // See https://github.com/LedgerHQ/ledgerjs/blob/master/packages/hw-transport/src/Transport.js
+  class Transport {
+    static create(openTimeout?: number, listenTimeout?: number): Promise<Transport>;
+
+    decorateAppAPIMethods(self: any, methods: string[], scrambleKey: string): void;
+    send(
+      cla: number,
+      ins: number,
+      p1: number,
+      p2: number,
+      data?: Buffer,
+      statusList?: number[],
+    ): Promise<Buffer>;
+  }
+}
---
+++ types/ledgerapp.d.ts
@@ -0,0 +1,35 @@
+/// <reference types="node" />
+/// <reference types="ledgerhq__hw-transport" />
+import { Transport } from "@ledgerhq/hw-transport";
+export interface LedgerAppErrorState {
+    readonly returnCode: number;
+    readonly errorMessage: string;
+}
+export interface LedgerAppVersion extends LedgerAppErrorState {
+    readonly testMode: boolean;
+    readonly major: number;
+    readonly minor: number;
+    readonly patch: number;
+    readonly deviceLocked: boolean;
+}
+export declare function isLedgerAppVersion(data: LedgerAppVersion | LedgerAppErrorState): data is LedgerAppVersion;
+export interface LedgerAppAddress extends LedgerAppErrorState {
+    readonly pubkey: Uint8Array;
+    readonly address: string;
+}
+export declare function isLedgerAppAddress(data: LedgerAppAddress | LedgerAppErrorState): data is LedgerAppAddress;
+export interface LedgerAppSignature extends LedgerAppErrorState {
+    readonly signature: Uint8Array;
+}
+export declare function isLedgerAppSignature(data: LedgerAppSignature | LedgerAppErrorState): data is LedgerAppSignature;
+export declare class LedgerApp {
+    static serializeBIP32(addressIndex: number): Buffer;
+    static signGetChunks(addressIndex: number, message: Uint8Array): readonly Buffer[];
+    private static processErrorResponse;
+    private readonly transport;
+    constructor(transport: Transport);
+    getVersion(): Promise<LedgerAppVersion | LedgerAppErrorState>;
+    getAddress(addressIndex: number, requireConfirmation?: boolean): Promise<LedgerAppAddress | LedgerAppErrorState>;
+    sign(addressIndex: number, message: Buffer): Promise<LedgerAppSignature | LedgerAppErrorState>;
+    private signSendChunk;
+}
